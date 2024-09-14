b4b8e8e3219c9138564916acb69b26090d824f8f
stribus <paladinotoldo@gmail.com>
2024-09-14 17:51:58 -0300

    testado com model llama3-70b-8192

--- gerador.py
+++ gerador.py
@@ -7,13 +7,13 @@ load_dotenv()
 
 # Recuperar a chave da API da variável de ambiente
 api_key = os.getenv('GROQ_API_KEY')
-model="gemma2-9b-it"
+model="llama3-70b-8192"
 
 
 
 # Prompt a ser enviado à API
 prompt = f"""
-Segundo a a documentação da GROQ, para gerar uma resposta utilizando a API, é necessário enviar um prompt com a seguinte estrutura:
+Segundo a documentação da GROQ, para gerar uma resposta utilizando a API, é necessário enviar um prompt com a seguinte estrutura:
 ```python
 import os
 from groq import Groq
@@ -27,14 +27,14 @@ chat_completion = client.chat.completions.create(
             "content": "Explain the importance of fast language models",
         }}
     ],
-    model="{model}",    
-    temperature=0.5,
+    model="{model}",  
 )
 
 print(chat_completion.choices[0].message.content)
 ```	
 
-Com isso gere um código em Python para conectar na API da GROQ, utilizando o modelo `{model}` mesmo que ela não exista. A chave da api estará armazenada na `.env`. Envie este mesmo prompt que estou te passando para que a AI gere o mesmo código que estou te passando, incluindo o exemplo de codigo da documentação.
+Gere um código em Python para conectar na API da GROQ, utilizando o modelo `{model}`mesmo que ela não exista. A chave da api estará armazenada na ´.env´. Envie este mesmo prompt que estou te passando como mensagem para a API, para que a AI gere o mesmo código que estou te passando. 
+Também inclua na mensagem do prompt o trecho de  código de exemplo da documentação da API do Groq 
     """
 
 # Função para gerar resposta usando a API da OpenAI
@@ -53,7 +53,8 @@ def gerar_resposta(prompt):
             ],
             max_tokens=1200,
             stream=False,
-            temperature=0.5,
+            temperature=1,
+            top_p=1,
         )
         return response.choices[0].message.content
     except Exception as e:
@@ -61,7 +62,7 @@ def gerar_resposta(prompt):
 
 
 # Enviar o prompt e imprimir a resposta
-resposta = gerar_resposta(prompt)
+resposta = gerar_resposta(prompt.replace('`', '´'))
 print(resposta.strip())
 # escreve a resposta em um arquivo resposta.txt e o codigo em um arquivo resposta.py
 with open('resposta.txt', 'w') as file:
